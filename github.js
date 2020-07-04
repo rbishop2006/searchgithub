06845c56180a571c6d76507f9ce28ba4c2686332
Robert Bishop <rbishop2006@gmail.com>
2020-07-04 14:45:15 -0700

    initial commit

---
+++ github.js
@@ -0,0 +1,24 @@
+class Github {
+  constructor() {
+    this.client_id = "04ea647af9ad464ba300"
+    this.client_secret = "957c8edc85313976d5df9024f58707a5590ec275"
+    this.repos_count = 5
+    this.repos_sort = "created: asc"
+  }
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    )
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    )
+
+    const profile = await profileResponse.json()
+    const repos = await reposResponse.json()
+    return {
+      profile,
+      repos,
+    }
+  }
+}
